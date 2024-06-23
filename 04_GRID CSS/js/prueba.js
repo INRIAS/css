c87bf814b78414c6a73ae41e3a7d010d7308b6ef
INRIAS <inrialva@gmail.com>
2024-06-23 11:59:16 -0500

    Grid Name Line

---
+++ 04_GRID CSS/js/prueba.js
@@ -0,0 +1,10 @@
+const numeroSecreto=6;
+const numeroUsuario=prompt("Digite su numero");
+
+console.log(numeroUsuario);
+
+if(numeroUsuario==numeroSecreto){
+    alert("Contraseña Correcta")
+} else{
+    alert("Contraseña Incorrecta")
+}
